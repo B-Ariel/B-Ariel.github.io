f5fdaf8cefdbe7072dc4c19def39450f47c496a0
Breno Ariel <152634218+B-Ariel@users.noreply.github.com>
2025-11-27 21:34:22 -0300

    Add files via upload

---
+++ assets/components/web-component/footer/footer.js
@@ -0,0 +1,14 @@
+// Rodapé
+const CurrentYear = new Date().getFullYear();
+const MyFooter =
+`
+Licença MIT
+<br>
+Direitos autorais (©)
+<br>
+2025 - ${CurrentYear}
+<br>
+Breno Ariel
+`;
+
+document.getElementById("MyFooter").innerHTML = MyFooter;
