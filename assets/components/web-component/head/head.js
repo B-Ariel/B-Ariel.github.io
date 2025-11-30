96cbc936d2b842b724bf25e863bb3c9759284c30
Breno Ariel <152634218+B-Ariel@users.noreply.github.com>
2025-11-30 00:34:53 -0300

    Add files via upload

--- assets/components/web-component/head/head.js
+++ assets/components/web-component/head/head.js
@@ -1,6 +1,6 @@
 // Ponto de partida para endereços
 const base = document.createElement("base");
-base.href ="https://b-ariel.github.io/";
+base.href ="http://127.0.0.1:5500/";
 document.head.appendChild(base);
 
 // Linkagem do CSS
@@ -23,5 +23,4 @@ http://127.0.0.1:5500/
 
 /* Endereço hospedado:
 https://b-ariel.github.io/
-
 */
