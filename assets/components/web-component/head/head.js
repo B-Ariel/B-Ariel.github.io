b5bd9bc84eb5f677eb495c6cc41007d2f3140aa5
Breno Ariel <152634218+B-Ariel@users.noreply.github.com>
2025-12-09 23:41:47 -0300

    Add files via upload

---
+++ assets/components/web-component/head/head.js
@@ -0,0 +1,31 @@
+// Ponto de partida para endereços
+const base = document.createElement("base");
+base.href ="http://127.0.0.1:5500/";
+document.head.appendChild(base);
+
+// Link do CSS
+const linkCSS = document.createElement('link');
+linkCSS.rel = 'stylesheet';
+linkCSS.href = '/assets/css/styles.css';
+document.head.appendChild(linkCSS);
+
+// Link do ícone
+const linkIcon = document.createElement('link');
+linkIcon.rel = 'icon';
+linkIcon.href = '/assets/img/icones/my-logo/my-mark-black.png';
+linkIcon.type = 'image/png';
+document.head.appendChild(linkIcon);
+
+// Link do PRISMJS
+const linkPrismjs = document.createElement('link');
+linkPrismjs.rel = 'stylesheet';
+linkPrismjs.href = '/assets/css/prismjs/prism-minified-version.css';
+document.head.appendChild(linkPrismjs);
+
+/* Endereço local:
+http://127.0.0.1:5500/
+*/
+
+/* Endereço hospedado:
+https://b-ariel.github.io/
+*/
